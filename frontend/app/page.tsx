08b192133f769ea97a7857d1d243eda7febf8afc
Snehal Shyamsukha <snehal20000@gmail.com>
2024-10-18 04:17:51 +0530

    discovery page completed

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,112 +1,28 @@
 "use client"
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from "next/image";
 import { getAllGames } from "./_actions/game";
+import { gameList } from "@/utils/gameData";
+import { useSearch } from './contexts/SearchContext';
+
 // import { useQuery } from "@tanstack/react-query";
 
 const menuItems = [
   { label: "All" },
   { label: "RPG" },
   { label: "TCG" },
-  { label: "Multiplayer" },
-  { label: "Strategy" }
+  { label: "P2E" },
+  { label: "CASUAL" },
+  { label: "SIMULATION" }
 ]
-export const gameList = [
-  {
-    id: 1,
-    name: "Dawnshard",
-    type: "TCG",
-    desc: "Free-to-play multiplayer card game with unique, AI-generated decks.",
-    img: "/dawnlogo.png",
-    overview: "Dawnshard is a revolutionary TCG that combines traditional card gameplay with AI-generated decks, offering a unique experience every time you play.",
-    category: "Card Game",
-    images: ["/dawnshard1.png", "/dawnshard2.png", "/dawnshard3.png"]
-  },
-  {
-    id: 2,
-    name: "Anybody Problem",
-    type: "CASUAL GAME",
-    desc: "A casual cozy supercollider game with onchain physics.",
-    img: "/ap.png",
-    overview: "ANYBODY PROBLEM is a casual, cozy supercollider game built with onchain physics. Built on Base, it uses zero-knowledge (ZK) cryptography to deliver a secure and engaging experience. In ANYBODY PROBLEM, you can mint your wins and climb the leaderboard to collect one-of-a-kind celestial bodies. Each day presents a new “problem” (game), which you can collect as an NFT! These celestial bodies will appear in your wallet, and you can even play the game directly on OpenSea.",
-    category: "Puzzle",
-    images: ["/img5.png", "/img2.png","/img3.png","/img4.png"]
-  },
-  {
-    id: 3,
-    name: "EthXY",
-    type: "MMORPG",
-    desc: "An MMORPG where you build your hero and go on grand adventures.",
-    img: "/ethxy.png",
-    overview: "EthXY is an expansive MMORPG set in a vast blockchain-powered world, where players can create unique heroes and embark on epic quests.",
-    category: "Role-Playing",
-    images: ["/ethxy1.png", "/ethxy2.png", "/ethxy3.png"]
-  },
-  {
-    id: 4,
-    name: "Ultimate Champions",
-    type: "E-SPORTS",
-    desc: "Collect, manage, and trade your favourite football players.",
-    img: "/uc.png",
-    overview: "Ultimate Champions brings the excitement of football management to the blockchain, allowing players to build and manage their dream teams.",
-    category: "Sports",
-    images: ["/uc1.png", "/uc2.png", "/uc3.png"]
-  },
-  {
-    id: 5,
-    name: "Fren Pet",
-    type: "CREATURE COLLECTING",
-    desc: "Raise, train, and battle with your onchain fren!",
-    img: "/fren.png",
-    overview: "Fren Pet is a delightful creature-collecting game where players can nurture and evolve unique blockchain-based pets.",
-    category: "Simulation",
-    images: ["/fren1.png", "/fren2.png", "/fren3.png"]
-  },
-  {
-    id: 6,
-    name: "Fit Club",
-    type: "PLAY TO EARN",
-    desc: "Workout, compete, and earn on your onchain fitness journey.",
-    img: "/fc.png",
-    overview: "Fit Club gamifies your fitness routine, rewarding real-world exercise with in-game progress and cryptocurrency earnings.",
-    category: "Health & Fitness",
-    images: ["/fc1.png", "/fc2.png", "/fc3.png"]
-  },
-  {
-    id: 7,
-    name: "CityVerse Tycoon",
-    type: "SIMULATION",
-    desc: "Buy, sell, and manage properties in a virtual, onchain city.",
-    img: "/cvt.png",
-    overview: "CityVerse Tycoon lets players become virtual real estate moguls, managing properties and economies in a blockchain-based metropolis.",
-    category: "Strategy",
-    images: ["/cvt1.png", "/cvt2.png", "/cvt3.png"]
-  },
-  {
-    id: 8,
-    name: "Everdawn",
-    type: "SIMULATION",
-    desc: "Free-to-Play, 3D Tactical Trading Card Game.",
-    img: "/ec.png",
-    overview: "Everdawn combines tactical gameplay with collectible card mechanics in a stunning 3D environment, all powered by blockchain technology.",
-    category: "Strategy",
-    images: ["/ec1.png", "/ec2.png", "/ec3.png"]
-  }
-]
-const gameList2 = [
-  { name: "Pixotchi", type: "FARMING", desc: "Grow, strategize, and earn rewards as you cultivate your garden on Base!", img: "/p.jpeg" },
-  { name: "Cat Town", type: "CASUAL GAME", desc: "Cozy, onchain game inspired by Animal Crossing.", img: "/cat.jpeg" },
-  { name: "Atari X", type: "CLASSIC", desc: "Play Atari classics like Asteroids and Breakout onchain.", img: "/atari.jpeg" },
-  { name: "GangstaVerse", type: "P2E", desc: "Next-gen digital collectible, Multi-chain gangster City", img: "/gv.jpeg" },
-]
+
 
 export default function Home() {
   const [selectedMenuItem, setSelectedMenuItem] = useState('All');
   const [games, setGames] = useState<Game[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const { searchTerm } = useSearch();
 
   useEffect(() => {
     const fetchGames = async () => {
@@ -125,8 +41,64 @@ export default function Home() {
     fetchGames();
   }, []);
 
+  const filteredGames = gameList.filter(game =>
+    game.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    game.type.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const renderGameCard = (item: any) => (
+    <Link
+      key={item.id}
+      href={`/games/${item.id}`}
+      className="w-1/4 py-4 px-2"
+    >
+      <div className="cursor-pointer transition-transform duration-300 hover:scale-105">
+        <Image
+          src={item.img}
+          alt={item.name}
+          width={279.576}
+          height={279.576}
+          className="object-cover rounded-[18.763px] mb-4"
+        />
+        <span className="text-[#AEFE03] ml-2 font-sans text-[23.454px] font-semibold">
+          {item.name}
+        </span>
+        <div className="flex items-center mt-1 mb-1 ml-2">
+          <span className="inline-block px-2 bg-[#8CFF05] text-[#012583] font-sans text-[16.796px] font-semibold leading-[24.752px] rounded-full">
+            {item.type}
+          </span>
+        </div>
+        <p className="text-white ml-2 font-sans text-[19.578px]">
+          {item.desc}
+        </p>
+      </div>
+    </Link>
+  );
+
   return (
     <div className="font-akira flex flex-col">
+            <div className="flex-grow relative overflow-hidden">
+        {searchTerm ? (
+          <section className="relative z-10 flex flex-col justify-start mt-10">
+            <div className="font-sans flex flex-col items-center justify-center w-full">
+              <div className="w-full max-w-[1305px] px-4">
+                <h2 className="text-[#AEFE03] font-sans text-[30px] font-extrabold mb-4">
+                  Search Results
+                </h2>
+                <div className="w-full h-auto rounded-[18.763px] border-2 border-[#AEFE03] bg-[#001894] flex flex-col justify-start p-4">
+                  <div className="flex flex-wrap justify-center">
+                    {filteredGames.length > 0 ? (
+                      filteredGames.map(renderGameCard)
+                    ) : (
+                      <p className="text-white text-center">No games found matching your search.</p>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        ) :(
+          <>
       <div className="flex-grow relative overflow-hidden">
         <div className="absolute inset-0 pt-3">
           <Image
@@ -314,7 +286,7 @@ export default function Home() {
                     TRENDING
                   </span>
                   <div className="flex flex-wrap justify-center">
-                    {gameList.map((item, index) => (
+                    {gameList.filter(game => selectedMenuItem === 'All' || game.type === selectedMenuItem).slice(0, 8).map((item, index) => (
                       <Link
                         key={index}
                         href={`/games/${item.id}`}
@@ -350,35 +322,38 @@ export default function Home() {
           <section className="relative z-10 flex flex-col justify-start mt-20 mb-10">
             <div className="font-sans flex flex-col items-center justify-center w-full">
               <div className="w-full max-w-[1305px] px-4">
-                <div className="w-full h-[529px] rounded-[18.763px] border-2 border-[#AEFE03] bg-[#001894] flex flex-col justify-start p-4">
+                <div className="w-full h-auto rounded-[18.763px] border-2 border-[#AEFE03] bg-[#001894] flex flex-col justify-start p-4">
                   <span className="text-[#AEFE03] font-sans ml-4 text-[30px] font-extrabold">
                     LATEST RELEASES
                   </span>
                   <div className="flex flex-row space-x-8 justify-center">
-                    {gameList2.map((item, index) => (
-                      <div className="py-4 h-auto">
-                        <div className="w-[279.576px] h-[279.576px] relative mb-2">
-
-                        <Image
-                          key={index}
-                          src={item.img}
-                          alt={item.name}
-                          fill
-                          className="object-cover rounded-[18.763px]"
-                        />
-                        </div>
-                        <span className="text-[#AEFE03] ml-2 font-sans text-[23.454px] font-semibold">
-                          {item.name}
-                        </span>
-                        <div className="flex items-center mt-1 mb-1 ml-2">
+                  {gameList.slice(8).map((item, index) => (
+                      <Link
+                        key={index}
+                        href={`/games/${item.id}`}
+                        className="w-1/4 py-4 px-2"
+                      >
+                        <div className="cursor-pointer transition-transform duration-300 hover:scale-105">
+                          <Image
+                            src={item.img}
+                            alt={item.name}
+                            width={279.576}
+                            height={279.576}
+                            className="object-cover rounded-[18.763px] mb-4"
+                          />
+                          <span className="text-[#AEFE03] ml-2 font-sans text-[23.454px] font-semibold">
+                            {item.name}
+                          </span>
+                          <div className="flex items-center mt-1 mb-1 ml-2">
                             <span className="inline-block px-2 bg-[#8CFF05] text-[#012583] font-sans text-[16.796px] font-semibold leading-[24.752px] rounded-full">
                               {item.type}
                             </span>
                           </div>
-                        <p className="text-white ml-2 font-sans text-[21.578px]">
-                          {item.desc}
-                        </p>
-                      </div>
+                          <p className="text-white ml-2 font-sans text-[19.578px]">
+                            {item.desc}
+                          </p>
+                        </div>
+                      </Link>
                     ))}
                   </div>
                 </div>
@@ -387,6 +362,9 @@ export default function Home() {
           </section>
         </div>
       </div>
+      </>
+        )}
+    </div>
     </div>
   );
 }
